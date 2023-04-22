a80a91709687cfd9005c56012f5b283392a0dfd2
wcasttro <wcastro.santos10@gmail.com>
2023-04-22 19:52:52 -0300

    adicionando projeto criando e manipulando view

---
+++ CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  CreatingAndCombiningViews
+//
+//  Created by werthen castro on 22/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack (alignment: .leading) {
+                Text("Turle Rock")
+                    .font(.title)
+                    .foregroundColor(.black)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }.padding()
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreatingAndCombiningViews/CreatingAndCombiningViews/CreatingAndCombiningViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAndCombiningViewsApp.swift
+//  CreatingAndCombiningViews
+//
+//  Created by werthen castro on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAndCombiningViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
